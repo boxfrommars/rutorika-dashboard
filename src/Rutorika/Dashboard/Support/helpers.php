7aa7cb98659ee0e1b1380be348b6dbf578ff7e20
Dmitry Groza <boxfrommars@gmail.com>
2014-11-05 09:27:32 +0300

    array_find_where helper

--- src/Rutorika/Dashboard/Support/helpers.php
+++ src/Rutorika/Dashboard/Support/helpers.php
@@ -1,8 +1,23 @@
 <?php
-if (!function_exists('app_array_find_where')) {
 
-    function app_array_find_where($name, $parameters = [])
+if (!function_exists('app_array_find')) {
+
+    function app_array_find($array, $key, $value)
     {
-        return app('url')->action($name, $parameters);
+        $results = array_where($array, function ($i, $item) use ($key, $value) {
+            return $item[$key] === $value;
+        });
+
+        return $results;
     }
 }
+
+if (!function_exists('app_array_find_where')) {
+
+    function app_array_find_where($array, $key, $value)
+    {
+        $results = app_array_find($array, $key, $value);
+        return array_shift($results);
+    }
+}
+
