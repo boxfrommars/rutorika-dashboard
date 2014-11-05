9326d51e617488424e22687ceee549faae5b9bf7
Dmitry Groza <boxfrommars@gmail.com>
2014-11-05 09:37:46 +0300

    grid helpers

--- src/Rutorika/Dashboard/Support/helpers.php
+++ src/Rutorika/Dashboard/Support/helpers.php
@@ -4,13 +4,11 @@ if (!function_exists('app_array_find')) {
 
     function app_array_find($array, $key, $value)
     {
-        \Log::debug($array);
-        \Log::debug($key);
-        \Log::debug($value);
-        $results = array_where($array, function ($i, $item) use ($key, $value) {
+        $results = array_where($array,
+        function ($i, $item) use ($key, $value) {
             return $item[$key] === $value;
         });
-        \Log::debug($results);
+
         return $results;
     }
 }
@@ -19,12 +17,41 @@ if (!function_exists('app_array_find_where')) {
 
     function app_array_find_where($array, $key, $value)
     {
-        \Log::debug($array);
-        \Log::debug($key);
-        \Log::debug($value);
         $results = app_array_find($array, $key, $value);
-        \Log::debug($results);
+
         return array_shift($results);
+    }
+}
+
+if (!function_exists('grid_link')) {
+
+    function grid_link($name, $action, $routeParams)
+    {
+        switch ($action) {
+            case 'view': // no break
+            case 'edit':
+                $icon = 'pencil';
+                break;
+            case 'destroy':
+                $icon = 'remove';
+                break;
+            default:
+                $icon = null;
+        }
+
+        $linkHtml = $icon ? '<span class="glyphicon glyphicon-' . $icon . '"></span>' : $action;
+
+        return '<a data-action="' . $action . '" href="' . route(".{$name}.{$action}",
+            $routeParams) . '">' . $linkHtml . '</a>';
+    }
+}
+
+if (!function_exists('add_link')) {
+    function add_link($name, $text = 'Добавить')
+    {
+        return '<a href="' . route(".{$name}.create") . '" class="btn btn-primary" role="button">
+                <span class="glyphicon glyphicon-plus"></span> ' . $text . '
+            </a>';
     }
 }
 
