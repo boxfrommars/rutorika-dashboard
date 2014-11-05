d0abffdf875913a6f031db3ff26ccbae73343833
Dmitry Groza <boxfrommars@gmail.com>
2014-11-05 09:33:12 +0300

    debug

--- src/Rutorika/Dashboard/Support/helpers.php
+++ src/Rutorika/Dashboard/Support/helpers.php
@@ -4,10 +4,13 @@ if (!function_exists('app_array_find')) {
 
     function app_array_find($array, $key, $value)
     {
+        \Log::debug($array);
+        \Log::debug($key);
+        \Log::debug($value);
         $results = array_where($array, function ($i, $item) use ($key, $value) {
             return $item[$key] === $value;
         });
-
+        \Log::debug($results);
         return $results;
     }
 }
@@ -16,7 +19,11 @@ if (!function_exists('app_array_find_where')) {
 
     function app_array_find_where($array, $key, $value)
     {
+        \Log::debug($array);
+        \Log::debug($key);
+        \Log::debug($value);
         $results = app_array_find($array, $key, $value);
+        \Log::debug($results);
         return array_shift($results);
     }
 }
