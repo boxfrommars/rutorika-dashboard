b03c82436509c58423ecf5239303203cef288b16
Dmitry Groza <boxfrommars@gmail.com>
2014-11-05 09:06:32 +0300

    sidebar partial

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -29,12 +29,7 @@
 
     <div id="wrapper">
         <div id="sidebar-wrapper">
-            <ul class="nav sidebar-nav">
-                <li><a href="#">Lorem Ipsum</a></li>
-                <li><a href="#">Dolor Sit Amet</a></li>
-                <li class="nav-divider"></li>
-                <li><a href="#">Consectetur</a></li>
-            </ul>
+            @include('dashboard::partials.sidebar')
         </div>
 
         <div id="page-content-wrapper">
---
+++ src/views/partials/sidebar.blade.php
@@ -0,0 +1,6 @@
+<ul class="nav sidebar-nav">
+    <li><a href="#">Lorem Ipsum</a></li>
+    <li><a href="#">Dolor Sit Amet</a></li>
+    <li class="nav-divider"></li>
+    <li><a href="#">Consectetur</a></li>
+</ul>
